105909ffd815c2bf710132b4af40c0451cf4f5b8
Charles Dubois <charles.dubois031@gmail.com>
2019-02-18 15:12:56 +0100

    creation de vue

--- C/machine learning/interface/test.c
+++ C/machine learning/interface/test.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(){
+	FILE* doc;
+	doc=fopen("views/doc.html", "w");
+	fprintf(doc, "despacito\n");
+	fclose(doc);
 	printf("ok\n");
 	return 0;
 }
--- C/machine learning/interface/test3.js
+++ C/machine learning/interface/test3.js
@@ -1,9 +1,25 @@
-var execFile = require('child_process').execFile;
+//-----------------exe----------------
 
-const child = execFile('test.exe', [], (error, stdout, stderr) => {
-    if (error) {
-        console.error('stderr', stderr);
-        throw error;
-    }
-    console.log('stdout', stdout);
+var execFile = require("child_process").execFile;
+
+const child = execFile("test.exe", [], (error, stdout, stderr) => {
+	if (error) {
+		console.error("stderr", stderr);
+		throw error;
+	}
+	console.log("stdout", stdout);
 });
+
+//----------------http----------------
+
+var http = require("http");
+
+var instructionsNouveauVisiteur = function(req, res) {
+	res.writeHead(200, {"Content-Type": "text/html"});
+	res.render(stdout);
+	res.end()
+}
+
+var server = http.createServer(instructionsNouveauVisiteur);
+
+server.listen(8080);
